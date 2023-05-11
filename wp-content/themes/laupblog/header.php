9d5963bf1e0a13b78ec062478443e570408e473e
LaupWing <loc_strict@hotmail.com>
2023-05-11 13:47:46 +0200

    update

--- wp-content/themes/laupblog/header.php
+++ wp-content/themes/laupblog/header.php
@@ -6,8 +6,8 @@
    <?php wp_head() ?>
 </head>
 <body <?php body_class() ?>>
-   <header class="bg-red-400 py-4 flex justify-center">
+   <header class="py-4 flex justify-center">
       <div class="container">
-         <h1 class="font-bold">LaupWing</h1>
+         <h1 class="font-bold text-white">LaupWing</h1>
       </div>
    </header>
