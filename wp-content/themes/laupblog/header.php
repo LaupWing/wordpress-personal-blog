41d5760df3f5164d9e0f67ed230973db6ff30612
LaupWing <loc_strict@hotmail.com>
2023-05-11 13:59:49 +0200

    udpate

--- wp-content/themes/laupblog/header.php
+++ wp-content/themes/laupblog/header.php
@@ -9,5 +9,12 @@
    <header class="py-4 flex justify-center">
       <div class="container">
          <h1 class="font-bold text-white">LaupWing</h1>
+         <nav>
+            <?php
+               wp_nav_menu([
+                  "theme_location" => "headerMenuLocation"
+               ]);
+            ?>
+         </nav>
       </div>
    </header>
