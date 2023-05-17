0169c91598b8ec53db513c02fac17827077a5e5f
LaupWing <loc_strict@hotmail.com>
2023-05-17 17:48:32 +0200

    update

--- wp-content/themes/laupblog/index.php
+++ wp-content/themes/laupblog/index.php
@@ -1,16 +1,15 @@
 <?php 
    get_header()
 ?>
-   <div class="container grid grid-cols-3">
+   <div class="container grid grid-cols-3 gap-4">
       <?php 
          while(have_posts()){
             the_post();
             $index = $wp_query->current_post;
       ?>
-         <p> <?php 
-            echo $index; 
-            the_title() 
-         ?> </p>
+         <div class="bg-red-200 <?php echo $index === 0 ? 'col-span-2 aspect-square' : 'col-span-1' ?>">
+            <p> <?php the_title() ?> </p>
+         </div>
       <?php 
          } 
       ?>
