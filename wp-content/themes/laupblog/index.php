1d76976cb9c9b4bf3f021874a241e931cb59a776
LaupWing <loc_strict@hotmail.com>
2023-05-17 18:02:50 +0200

    udpate

--- wp-content/themes/laupblog/index.php
+++ wp-content/themes/laupblog/index.php
@@ -1,15 +1,27 @@
 <?php 
    get_header()
 ?>
-   <div class="container grid grid-cols-3 gap-4">
+   <div class="container grid grid-cols-2 grid-rows-2 gap-4">
       <?php 
          while(have_posts()){
             the_post();
             $index = $wp_query->current_post;
       ?>
-         <div class="bg-red-200 <?php echo $index === 0 ? 'col-span-2 aspect-square' : 'col-span-1' ?>">
-            <p> <?php the_title() ?> </p>
-         </div>
+         <?php if($index === 0) {?>
+            <div class="bg-red-200 rounded-2xl col-span-1 row-span-2 aspect-square">
+               <p> <?php the_title() ?> </p>
+            </div>
+         <?php }?>
+         <?php if($index > 0 && $index <= 2) {?>
+            <div class="bg-red-200 rounded-2xl col-span-1 row-span-1">
+               <p> <?php the_title() ?> </p>
+            </div>
+         <?php }?>
+         <?php if($index > 2) {?>
+            <div class="bg-red-200 rounded-2xl aspect-video col-span-1 row-span-1">
+               <p> <?php the_title() ?> </p>
+            </div>
+         <?php }?>
       <?php 
          } 
       ?>
