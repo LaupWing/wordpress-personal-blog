4afa824cc55f6efad3df5828ddacea74f1eef60c
LaupWing <loc_strict@hotmail.com>
2023-05-18 08:37:25 +0200

    added shadow

--- wp-content/themes/laupblog/index.php
+++ wp-content/themes/laupblog/index.php
@@ -11,7 +11,7 @@
             $index = $wp_query->current_post;
       ?>
          <?php if($index === 0) {?>
-            <div class="relative rounded-2xl col-span-1 overflow-hidden row-span-2 aspect-square flex items-start flex-col p-4 justify-start">
+            <div class="relative rounded-2xl col-span-1 overflow-hidden row-span-2 aspect-square flex items-start flex-col p-4 justify-start shadow">
                <div class="w-full flex justify-between items-start">
                   <p class="px-2 bg-white rounded-full font-bold text-xs py-0.5"><?php the_time("M d, Y"); ?></p>
                   <p class="bg-white px-4 py-2 rounded-md font-bold text-3xl"> <?php the_title() ?> </p>
@@ -23,7 +23,7 @@
             </div>
          <?php }?>
          <?php if($index > 0 && $index <= 2) {?>
-            <div class="relative rounded-2xl overflow-hidden col-span-1 row-span-1 flex items-start flex-col p-4 justify-start">
+            <div class="relative rounded-2xl overflow-hidden col-span-1 row-span-1 flex items-start flex-col p-4 justify-start shadow">
                <div class="w-full flex justify-between items-start">
                   <p class="bg-white px-4 py-2 rounded-md font-bold text-xl"> <?php the_title() ?> </p>
                   <p class="px-2 bg-white rounded-full font-bold text-xs py-0.5"><?php the_time("M d, Y"); ?></p>
@@ -40,7 +40,7 @@
             </div>
          <?php }?>
          <?php if($index > 2) {?>
-            <div class="relative overflow-hidden rounded-2xl aspect-video col-span-1 row-span-1 flex items-start flex-col p-4 justify-start">
+            <div class="relative overflow-hidden rounded-2xl aspect-video col-span-1 row-span-1 flex items-start flex-col p-4 justify-start shadow">
                <div class="w-full flex justify-between items-start">
                   <p class="bg-white px-4 py-2 rounded-md font-bold text-xl"> <?php the_title() ?> </p>
                   <p class="px-2 bg-white rounded-full font-bold text-xs py-0.5"><?php the_time("M d, Y"); ?></p>
