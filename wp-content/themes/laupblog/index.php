f095ff2569084b09cf12b9a5644cc50d126a2faa
LaupWing <loc_strict@hotmail.com>
2023-05-18 08:49:49 +0200

    the permalink

--- wp-content/themes/laupblog/index.php
+++ wp-content/themes/laupblog/index.php
@@ -44,21 +44,23 @@
             </div>
          <?php }?>
          <?php if($index > 2) {?>
-            <div class="relative overflow-hidden rounded-2xl aspect-video col-span-1 row-span-1 flex items-start flex-col p-4 justify-start shadow">
-               <div class="w-full flex justify-between items-start">
-                  <p class="bg-white px-4 py-2 rounded-md font-bold text-xl"> <?php the_title() ?> </p>
-                  <p class="px-2 bg-white rounded-full font-bold text-xs py-0.5"><?php the_time("M d, Y"); ?></p>
+            <a href="<?php the_permalink() ?>">
+               <div class="relative overflow-hidden rounded-2xl aspect-video col-span-1 row-span-1 flex items-start flex-col p-4 justify-start shadow">
+                  <div class="w-full flex justify-between items-start">
+                     <p class="bg-white px-4 py-2 rounded-md font-bold text-xl"> <?php the_title() ?> </p>
+                     <p class="px-2 bg-white rounded-full font-bold text-xs py-0.5"><?php the_time("M d, Y"); ?></p>
+                  </div>
+                  <div class="w-8 h-8 m-4 flex justify-center items-center bg-white rounded-full">
+                     <?php 
+                        $custom_class = "w-4 h-4";
+                        get_template_part("templates/icons/arrowRightUp", null, array(
+                           "custom_class" => $custom_class
+                        ));
+                     ?>
+                  </div>
+                  <img class="inset-0 absolute w-full h-full object-cover -z-10" src="<?php echo get_the_post_thumbnail_url() ?>" alt="">
                </div>
-               <div class="w-8 h-8 m-4 flex justify-center items-center bg-white rounded-full">
-                  <?php 
-                     $custom_class = "w-4 h-4";
-                     get_template_part("templates/icons/arrowRightUp", null, array(
-                        "custom_class" => $custom_class
-                     ));
-                  ?>
-               </div>
-               <img class="inset-0 absolute w-full h-full object-cover -z-10" src="<?php echo get_the_post_thumbnail_url() ?>" alt="">
-            </div>
+            </a>
          <?php }?>
       <?php 
          } 
