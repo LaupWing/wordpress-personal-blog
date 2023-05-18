0441c7ed37c4b666a4fa77e7ac34790d3a1a5768
LaupWing <loc_strict@hotmail.com>
2023-05-18 09:13:10 +0200

    update

---
+++ wp-content/themes/laupblog/single.php
@@ -0,0 +1,14 @@
+<?php 
+   get_header()
+?>
+   <div class="container flex flex-col">
+      <div class="w-full h-44 rounded-lg relative overflow-hidden p-4 flex items-start justify-start">
+         <h2 class="mb-3 text-4xl font-bold italic bg-white px-4 py-2 rounded-md">
+            <?php the_title() ?>
+         </h2>
+         <img class="absolute inset-0 w-full h-full object-cover -z-10" src="<?php echo get_the_post_thumbnail_url() ?>" alt="">
+      </div>
+   </div>
+<?php 
+   get_footer()
+?>
