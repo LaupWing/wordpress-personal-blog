91869fbb39980b5bcd6b4404be11b9d9769aaea4
LaupWing <loc_strict@hotmail.com>
2023-05-18 08:46:32 +0200

    The permalink

--- wp-content/themes/laupblog/index.php
+++ wp-content/themes/laupblog/index.php
@@ -11,16 +11,18 @@
             $index = $wp_query->current_post;
       ?>
          <?php if($index === 0) {?>
-            <div class="relative rounded-2xl col-span-1 overflow-hidden row-span-2 aspect-square flex items-start flex-col p-4 justify-start shadow">
-               <div class="w-full flex justify-between items-start">
-                  <p class="px-2 bg-white rounded-full font-bold text-xs py-0.5"><?php the_time("M d, Y"); ?></p>
-                  <p class="bg-white px-4 py-2 rounded-md font-bold text-3xl"> <?php the_title() ?> </p>
+            <a href="<?php the_permalink() ?>">
+               <div class="relative rounded-2xl col-span-1 overflow-hidden row-span-2 aspect-square flex items-start flex-col p-4 justify-start shadow">
+                  <div class="w-full flex justify-between items-start">
+                     <p class="px-2 bg-white rounded-full font-bold text-xs py-0.5"><?php the_time("M d, Y"); ?></p>
+                     <p class="bg-white px-4 py-2 rounded-md font-bold text-3xl"> <?php the_title() ?> </p>
+                  </div>
+                  <div class="mt-auto ml-auto w-10 h-10 flex justify-center items-center bg-white rounded-full">
+                     <?php get_template_part("templates/icons/arrowRightUp")?>
+                  </div>
+                  <img class="inset-0 absolute w-full h-full object-cover -z-10" src="<?php echo get_the_post_thumbnail_url() ?>" alt="">
                </div>
-               <div class="mt-auto ml-auto w-10 h-10 flex justify-center items-center bg-white rounded-full">
-                  <?php get_template_part("templates/icons/arrowRightUp")?>
-               </div>
-               <img class="inset-0 absolute w-full h-full object-cover -z-10" src="<?php echo get_the_post_thumbnail_url() ?>" alt="">
-            </div>
+            </a>
          <?php }?>
          <?php if($index > 0 && $index <= 2) {?>
             <div class="relative rounded-2xl overflow-hidden col-span-1 row-span-1 flex items-start flex-col p-4 justify-start shadow">
