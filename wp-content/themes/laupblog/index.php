66ffcea1daf2bb637ee8df5c5246f4ef13c2ae67
LaupWing <loc_strict@hotmail.com>
2023-05-18 07:36:08 +0200

    update

--- wp-content/themes/laupblog/index.php
+++ wp-content/themes/laupblog/index.php
@@ -13,8 +13,8 @@
          <?php if($index === 0) {?>
             <div class="relative rounded-2xl col-span-1 overflow-hidden row-span-2 aspect-square flex items-start p-4 justify-start">
                <div class="w-full flex justify-between items-start">
-                  <p class="px-2 bg-white rounded-full"><?php echo the_time("n.j.y"); ?></p>
-                  <p class="bg-white p-2 rounded-md font-bold text-xl"> <?php the_title() ?> </p>
+                  <p class="px-2 bg-white rounded-full font-bold text-xs py-0.5"><?php the_time("M d, Y"); ?></p>
+                  <p class="bg-white px-4 py-2 rounded-md font-bold text-2xl"> <?php the_title() ?> </p>
                </div>
                <img class="inset-0 absolute w-full h-full object-cover -z-10" src="<?php echo get_the_post_thumbnail_url() ?>" alt="">
             </div>
