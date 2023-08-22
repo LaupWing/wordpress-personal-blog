65c60d7cdc78e69f777c335a3e495a1c7708ea67
LaupWing <loc_strict@hotmail.com>
2023-08-22 19:57:43 +0200

    udpated

--- wp-content/themes/laupblog/src/sidenav.ts
+++ wp-content/themes/laupblog/src/sidenav.ts
@@ -1,6 +1,8 @@
 export default class Sidenav {
+   close_btn = HTMLButtonElement
+   
    constructor()
    {
-      
+      this.close_btn = document.querySelector("button") as HTMLButtonElement
    }  
 }
