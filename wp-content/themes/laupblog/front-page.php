54b281c8441291123da5cd167db19d65afc49f42
LaupWing <loc_strict@hotmail.com>
2023-08-27 16:46:52 +0200

    moved

--- wp-content/themes/laupblog/front-page.php
+++ wp-content/themes/laupblog/front-page.php
@@ -20,26 +20,7 @@
             $index = $homepagePosts->current_post;
       ?>
          <?php if($index === 0) {?>
-            <a 
-               href="<?php the_permalink() ?>"
-               class="col-span-1 row-span-2 aspect-square blog-card"
-            >
-               <div class="w-full flex justify-between items-start">
-                  <p class="px-2 bg-white rounded-full font-bold text-xs py-0.5"><?php the_time("M d, Y"); ?></p>
-                  <p class="bg-white px-4 py-2 rounded-md font-bold text-3xl"> <?php the_title() ?> </p>
-               </div>
-               <p class="text-right bg-white/40 backdrop-blur-md mt-2 rounded ml-auto p-2 text-sm max-w-[80%] font-bold">
-                  <?php echo get_the_excerpt() ?>
-               </p>
-               <div class="mt-auto ml-auto w-10 h-10 flex justify-center items-center bg-white rounded-full">
-                  <?php get_template_part("templates/icons/arrowRightUp")?>
-               </div>
-               <img 
-                  class="inset-0 absolute w-full h-full object-cover -z-10" 
-                  src="<?php echo get_the_post_thumbnail_url() ?>" 
-                  alt="Thumbnail photo"
-               >
-            </a>
+            <?php get_template_part("templates/cards/blog-card-big")?>
          <?php }?>
          <?php if($index > 0 && $index <= 2) {?>
             <a 
