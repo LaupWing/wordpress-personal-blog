abc5cde017552bcd9e3934c8f13e8c48e54d4364
LaupWing <loc_strict@hotmail.com>
2023-08-27 16:03:36 +0200

    ADDED MENU ITEM

--- wp-content/themes/laupblog/src/sidenav.ts
+++ wp-content/themes/laupblog/src/sidenav.ts
@@ -1,8 +1,10 @@
 export default class Sidenav {
    close_btn: HTMLButtonElement
+   menu_btn: HTMLButtonElement
    
    constructor()
    {
       this.close_btn = document.querySelector("button#close") as HTMLButtonElement
+      this.menu_btn = document.querySelector("button#menu-btn") as HTMLButtonElement
    }  
 }
