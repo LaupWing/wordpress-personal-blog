886902f4a9c08f8f1c0da65903d4d58dacfbfcd5
LaupWing <loc_strict@hotmail.com>
2023-08-27 16:04:20 +0200

    openMenu\

--- wp-content/themes/laupblog/src/sidenav.ts
+++ wp-content/themes/laupblog/src/sidenav.ts
@@ -6,5 +6,12 @@ export default class Sidenav {
    {
       this.close_btn = document.querySelector("button#close") as HTMLButtonElement
       this.menu_btn = document.querySelector("button#menu-btn") as HTMLButtonElement
+
+      this.menu_btn.addEventListener("click", this.openMenu)
    }  
+
+
+   private openMenu() {
+      
+   }
 }
