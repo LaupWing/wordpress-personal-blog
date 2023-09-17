31a7ce3035f3d3fee6696b90d5aeec566780c6d4
LaupWing <loc_strict@hotmail.com>
2023-09-17 17:03:40 +0200

    udpate

--- wp-content/themes/laupblog/footer.php
+++ wp-content/themes/laupblog/footer.php
@@ -1,11 +1,19 @@
       </main>
-      <footer>
-         <button 
-            class="ml-auto mb-6"
-            id="close"
-         >
-            <?php get_template_part("templates/icons/social-instagram")?>
-         </button>
+      <footer class="py-10 container flex flex-col mx-auto">
+         <div class="flex space-x-4">
+            <button>
+               <?php get_template_part("templates/icons/social-instagram")?>
+            </button>
+            <button>
+               <?php get_template_part("templates/icons/social-twitter")?>
+            </button>
+            <button>
+               <?php get_template_part("templates/icons/social-youtube")?>
+            </button>
+            <button>
+               <?php get_template_part("templates/icons/social-facebook")?>
+            </button>
+         </div>
       </footer>
       <?php wp_footer(); ?>
    </body>
