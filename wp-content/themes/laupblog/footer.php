46e75f23ff02bddc93a6a2ee36dee1e76055a6fb
LaupWing <loc_strict@hotmail.com>
2023-09-17 17:07:40 +0200

    udpate

--- wp-content/themes/laupblog/footer.php
+++ wp-content/themes/laupblog/footer.php
@@ -1,18 +1,21 @@
       </main>
       <footer class="py-10 container flex flex-col mx-auto">
          <div class="flex space-x-4">
-            <button>
+            <button class="text-yellow-400 hover:scale-105 duration-200 fill-current">
                <?php get_template_part("templates/icons/social-instagram")?>
             </button>
-            <button>
+            <button  class="text-yellow-400 hover:scale-105 duration-200 fill-current">
                <?php get_template_part("templates/icons/social-twitter")?>
             </button>
-            <button>
+            <button  class="text-yellow-400 hover:scale-105 duration-200 fill-current">
                <?php get_template_part("templates/icons/social-youtube")?>
             </button>
-            <button>
+            <button  class="text-yellow-400 hover:scale-105 duration-200 fill-current">
                <?php get_template_part("templates/icons/social-facebook")?>
             </button>
+         </div>
+         <div class="text-center py-4 mx-auto uppercase font-bold text-slate-300 text-sm">
+            © All Rights Reserved.
          </div>
       </footer>
       <?php wp_footer(); ?>
