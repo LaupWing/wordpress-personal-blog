9b5889fd58595a4495077be081de765356c52ad2
LaupWing <loc_strict@hotmail.com>
2023-09-17 16:32:48 +0200

    added first button

--- wp-content/themes/laupblog/footer.php
+++ wp-content/themes/laupblog/footer.php
@@ -1,4 +1,12 @@
       </main>
+      <footer>
+         <button 
+            class="ml-auto mb-6"
+            id="close"
+         >
+            <?php get_template_part("templates/icons/social-instagram")?>
+         </button>
+      </footer>
       <?php wp_footer(); ?>
    </body>
 </html>
